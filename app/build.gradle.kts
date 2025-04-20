343301b81d28a590b6cc635fb99e2a0a401e10a0
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-04-21 01:25:29 +0300

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
 }
 
 android {
     namespace = "com.nicos.sampleforegroundservice"
-    buildToolsVersion = "35.0.0"
+    buildToolsVersion = "35.0.1"
     compileSdk = 35
 
     defaultConfig {
