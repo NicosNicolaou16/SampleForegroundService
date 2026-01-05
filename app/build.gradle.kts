4b681e534d0572b2b45f6f68dee749c3b9dde1a3
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-06 01:19:02 +0200

    update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.activity:activity-compose:1.12.2")
-    implementation(platform("androidx.compose:compose-bom:2025.09.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
