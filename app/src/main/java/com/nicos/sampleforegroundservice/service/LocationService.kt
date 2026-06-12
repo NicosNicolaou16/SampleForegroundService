b8c53fed379612e757d5f69e7df545f01cf61931
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-12 22:56:17 +0300

    revert the code

--- app/src/main/java/com/nicos/sampleforegroundservice/service/LocationService.kt
+++ app/src/main/java/com/nicos/sampleforegroundservice/service/LocationService.kt
@@ -76,16 +76,6 @@ class LocationService : Service(), LocationListener {
     }
 
     private fun findTheLocationForSdkBiggerThan30(locationManager: LocationManager) {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
         locationManager.requestLocationUpdates(
             LocationManager.FUSED_PROVIDER,
             minTimeLocationUpdateInMillisecond,
@@ -108,16 +98,6 @@ class LocationService : Service(), LocationListener {
                 false
             )
             if (provider != null) {
-                if (ActivityCompat.checkSelfPermission(
-                        this@LocationService,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this@LocationService,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    return
-                }
                 locationManager.requestLocationUpdates(
                     provider,
                     minTimeLocationUpdateInMillisecond,
