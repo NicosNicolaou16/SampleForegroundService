5915cecdb2172dfb73bd803a2b3212bcbf29ab76
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-12 22:50:18 +0300

    update Kotlin version to 2.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0"
 }
 
 android {
